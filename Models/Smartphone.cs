3e4ae49b549c3916ce4bb563a957a7aaa106b0e4
Guilherme Rocha <GuiPython2001@gmail.com>
2023-11-28 17:30:10 -0300

    add: propriedades e métodos para classe smartphone

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -1,26 +1,29 @@
-namespace DesafioPOO.Models
+namespace DesafioPOO.Models;
+
+public abstract class Smartphone
 {
-    public abstract class Smartphone
+    public string Numero { get; set; }
+    private readonly string _modelo;
+    private readonly string _imei;
+    private readonly int _memoria;
+
+    protected Smartphone(string numero, string modelo, string imei, int memoria)
     {
-        public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
-
-        public Smartphone(string numero)
-        {
-            Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
-        }
-
-        public void Ligar()
-        {
-            Console.WriteLine("Ligando...");
-        }
-
-        public void ReceberLigacao()
-        {
-            Console.WriteLine("Recebendo ligação...");
-        }
-
-        public abstract void InstalarAplicativo(string nomeApp);
+        Numero = numero;
+        _modelo = modelo;
+        _imei = imei;
+        _memoria = memoria;
     }
+
+    public void Ligar()
+    {
+        Console.WriteLine("Ligando...");
+    }
+
+    public void ReceberLigacao()
+    {
+        Console.WriteLine($"{Numero} - {_modelo} - {_imei} - {_memoria} - recebendo ligação...");
+    }
+
+    public abstract void InstalarAplicativo(string nomeApp);
 }
