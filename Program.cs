50581277fa6a49bdf481693e829953f665d7010f
Guilherme Rocha <GuiPython2001@gmail.com>
2023-11-28 17:31:20 -0300

    add: função principal
    Testes com uso de coleções genéricas.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+var iphone = new Iphone("+55(11)99999-9999", "Iphone X", "1223-223", 128);
+var nokia = new Nokia("+55(21)5555-5555", "Nokia G21", "4555-566", 16);
+var smartphones = new List<Smartphone> { iphone, nokia };
+var apps = new List<string> { "Netflix", "Prime Video" };
+
+foreach (var (smartphone, app) in smartphones.Zip(apps))
+{
+    smartphone.Ligar();
+    smartphone.InstalarAplicativo(app);
+    smartphone.ReceberLigacao();
+    Console.WriteLine();
+}
